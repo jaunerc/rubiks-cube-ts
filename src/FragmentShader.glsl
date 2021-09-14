12bcfa7ea0bd17779cbad27114f844379d298db4
jaunerc <cyrill.jauner@zuehlke.com>
2021-09-14 10:58:36 +0200

    draw wireframe additional to the solid cube

--- src/FragmentShader.glsl
+++ src/FragmentShader.glsl
@@ -1,7 +1,12 @@
 precision mediump float;
 
 varying vec4 color;
+uniform vec4 wireFrameColor;
+uniform bool drawWireFrame;
 
 void main() {
     gl_FragColor = color;
+    if (drawWireFrame) {
+        gl_FragColor = wireFrameColor;
+    }
 }
